d6aef4d4979fde4f76b915de794ffce8976ddf01
Ethan Mulenga <garumidev@gmail.com>
2026-03-21 17:11:04 +0200

    working on the variables and data types

---
+++ day1_variables.py
@@ -0,0 +1,49 @@
+#variables (labeled containers that store data)
+#variable is a just a name you give to a piece of data so you can use it later.
+
+name = "Ethan"
+#print(name) note: This will print the value of the variable name, which is "Ethan".
+age = 18
+#print(name, age) note: This will print the two values in the same line.
+
+#Data Types(kinds of data)
+# intergers- whole numbers (positive and negative) (int)
+#Whole numbers 
+age = 20
+score = 100
+
+#print (age, score) note: This will print the values of age and score, which are 20 and 100 respectively.
+#used for;
+#- counting
+#- ordering
+#loops
+#indexing
+#calucations
+#where; 
+total = 10 + 15
+#print(total)
+#floats- numbers with decimal points (float)
+price = 19.99
+temperature = 36.5
+#print(price, temperature) note: This will print the values of price and temperature, which are 19.99 and 36.5 respectively.
+#used for;
+#- measurements
+#- money
+#- scientific data
+#where;
+total_temperature = 36.6 + 37.5
+#print(total_temperature) 
+#String (str)
+#Text Must be in qoutes.
+name = "Ethan"
+Message = "python huh.."
+#print(name, Message) note: This will print the values of name and Message, which are "Ethan" and "python huh.." respectively.
+#used for;
+#- names
+#- messages
+#- any text data
+#print("Hello", name) when you add the coma between "Hello" and name, it will print "Hello Ethan". If you remove the coma, it will print "HelloEthan" without a space. when you add the plus sign between "Hello" and name, it will print "HelloEthan" without a space. To add a space, you can use "Hello " with a space at the end, like this: "Hello " + name, which will print "HelloEthan".
+#boolean(bool)
+is_raining = True
+is_sunny = False
+print (is_raining)
