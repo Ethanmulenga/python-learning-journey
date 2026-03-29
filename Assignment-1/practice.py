b22b018ee7440936f8da0cc3785b84aa270e0e81
Ethanmulenga <mulengaethan0@gmail.com>
2026-03-29 14:04:10 +0200

    done with the practice on panda i will do it later

--- Assignment-1/practice.py
+++ Assignment-1/practice.py
@@ -28,7 +28,31 @@ df["amount"] = (
 # convert amount to float in a cleaner way.
 df["amount"] = pd.to_numeric(df["amount"], errors="coerce").fillna(0)
 
+#Date cleaner
+df["date"] = pd.to_datetime(df["date"], errors="coerce")
+df["date"] = df["date"].ffill()
+df["date"] =df["date"].bfill()
+
 # drop empty sections
 df = df.drop_duplicates()
 
+#totals spent
+total_spent = df["amount"].sum()
+average_spent = df["amount"].mean()
+max_amount = df["amount"].max()
+min_amount = df["amount"].min()
+
+# preping plotting data 
+category_totals = df.groupby("category")["amount"].sum().sort_values(ascending=False)
+daily_totals = df.groupby("dates")["amount"].sum()
+
+
 print(df)
+print(f"The Total Amount Spent: {total_spent}")
+print(f"Average Transaction: {average_spent}")
+print(f"Largest Transaction: {max_amount}")
+print(f"smallest Transaction: {min_amount}")
+print(category_totals)
+print(daily_totals)
+
+
